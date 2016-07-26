b08298affc69156d701fccbdbbf219a7c476c3b5
Nader Dabit <dabit3@gmail.com>
2016-07-26 10:22:46 -0500

    added rotating animation branch

---
+++ index.ios.js
@@ -0,0 +1,81 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+  Animated,
+  Image,
+  Easing
+} from 'react-native'
+
+class animations extends Component {
+  constructor () {
+    super()
+    this.state = {
+      spinValue: new Animated.Value(0)
+    }
+  }
+  componentDidMount () {
+    this.spin()
+  }
+  spin () {
+    this.state.spinValue.setValue(0)
+    Animated.timing(
+      this.state.spinValue,
+      {
+        toValue: 1,
+        duration: 4000,
+        easing: Easing.linear
+      }
+    ).start(() => this.spin())
+  }
+  render () {
+    const getStartValue = () => '0deg'
+    const getEndValue = () => '360deg'
+    const spin = this.state.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [getStartValue(), getEndValue()]
+    })
+    /* This also works, just using above example to show functions instead of strings /*
+    /*
+    const spin = this.state.spinValue.interpolate({
+       inputRange: [0, 1],
+       outputRange: ['0deg', '360deg']
+    }) */
+    return (
+      <Animated.View style={styles.container}>
+        <Animated.Image
+          style={{ width: 227, height: 200, transform: [{rotate: spin}] }}
+          source={{uri: 'https://s3.amazonaws.com/media-p.slid.es/uploads/alexanderfarennikov/images/1198519/reactjs.png'}}/>
+      </Animated.View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 28,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    fontSize: 19,
+    marginBottom: 5
+  }
+})
+
+AppRegistry.registerComponent('animations', () => animations)
