4cf3bd9c82783afba7c4ab298ea9a4f3e09a9338
Nader Dabit <dabit3@gmail.com>
2016-07-31 15:12:53 -0500

    added multiple timing examples

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,105 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  Animated,
+  Image,
+  Easing
+} from 'react-native'
 
 class animations extends Component {
-  render() {
+  constructor () {
+    super()
+    this.animatedValue = new Animated.Value(0)
+  }
+  componentDidMount () {
+    this.animate()
+  }
+  animate () {
+    this.animatedValue.setValue(0)
+    Animated.timing(
+      this.animatedValue,
+      {
+        toValue: 1,
+        duration: 2000,
+        easing: Easing.linear
+      }
+    ).start(() => this.animate())
+  }
+  render () {
+    const marginLeft = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 300]
+    })
+    const opacity = this.animatedValue.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [0, 1, 0]
+    })
+    const movingMargin = this.animatedValue.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [0, 300, 0]
+    })
+    const textSize = this.animatedValue.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [18, 32, 18]
+    })
+    const rotateX = this.animatedValue.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: ['0deg', '180deg', '0deg']
+    })
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Animated.View
+          style={{
+            marginLeft,
+            height: 30,
+            width: 40,
+            backgroundColor: 'red'}} />
+        <Animated.View
+          style={{
+            opacity,
+            marginTop: 10,
+            height: 30,
+            width: 40,
+            backgroundColor: 'blue'}} />
+        <Animated.View
+          style={{
+            marginLeft: movingMargin,
+            marginTop: 10,
+            height: 30,
+            width: 40,
+            backgroundColor: 'orange'}} />
+        <Animated.Text
+          style={{
+            fontSize: textSize,
+            marginTop: 10,
+            color: 'green'}} >
+            Animated Text!
+        </Animated.Text>
+        <Animated.View
+          style={{
+            transform: [{rotateX}],
+            marginTop: 50,
+            height: 30,
+            width: 40,
+            backgroundColor: 'black'}}>
+          <Text style={{color: 'white'}}>Hello from TransformX</Text>
+        </Animated.View>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    paddingTop: 150
+  }
+})
 
-AppRegistry.registerComponent('animations', () => animations);
+AppRegistry.registerComponent('animations', () => animations)
