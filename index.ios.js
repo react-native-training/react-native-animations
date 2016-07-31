6851a4aa84bfb798fbb30191b47df6dfb8ec1eaf
Nader Dabit <dabit3@gmail.com>
2016-07-31 18:08:45 -0500

    animated.parallel done

--- index.ios.js
+++ index.ios.js
@@ -1,33 +1,80 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  Animated,
+  View,
+  Easing,
+  TouchableHighlight,
+  Dimensions
+} from 'react-native'
+
+const { width } = Dimensions.get('window')
 
 class animations extends Component {
-  render() {
+
+  constructor () {
+    super()
+    this.animatedValue1 = new Animated.Value(0)
+    this.animatedValue2 = new Animated.Value(0)
+    this.animatedValue3 = new Animated.Value(0)
+  }
+
+  componentDidMount () {
+    this.animate()
+  }
+
+  animate () {
+    this.animatedValue1.setValue(0)
+    this.animatedValue2.setValue(0)
+    this.animatedValue3.setValue(0)
+    const createAnimation = function (value, duration, easing, delay = 0) {
+      return Animated.timing(
+       value,
+        {
+          toValue: 1,
+          duration,
+          easing,
+          delay
+        }
+      )
+    }
+    Animated.parallel([
+      createAnimation(this.animatedValue1, 2000, Easing.ease),
+      createAnimation(this.animatedValue2, 1000, Easing.ease, 1000),
+      createAnimation(this.animatedValue3, 1000, Easing.ease, 2000)
+    ]).start()
+  }
+
+  render () {
+    const scaleText = this.animatedValue1.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0.5, 2]
+    })
+    const spinText = this.animatedValue2.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '720deg']
+    })
+    const introButton = this.animatedValue3.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-100, 400]
+    })
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={[styles.container]}>
+        <Animated.View style={{ transform: [{scale: scaleText}] }}>
+          <Text>Welcome</Text>
+        </Animated.View>
+        <Animated.View style={{ marginTop: 20, transform: [{rotate: spinText}] }}>
+          <Text style={{fontSize: 20}}>to the App!</Text>
+        </Animated.View>
+        <Animated.View style={{top: introButton, position: 'absolute'}}>
+          <TouchableHighlight onPress={this.animate.bind(this)} style={styles.button}>
+            <Text style={{color: 'white', fontSize: 20}}>Click Here To Start</Text>
+          </TouchableHighlight>
+        </Animated.View>
       </View>
-    );
+    )
   }
 }
 
@@ -35,19 +82,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  button: {
+    width: width - 40,
+    height: 70,
+    marginLeft: 20,
+    marginRight: 20,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
-AppRegistry.registerComponent('animations', () => animations);
+AppRegistry.registerComponent('animations', () => animations)
